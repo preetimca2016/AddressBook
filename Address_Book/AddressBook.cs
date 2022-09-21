73b301733f33c6d14dad4ee1c7f88b3377beec8d
“preetimca2016” <preetimca2016@gmail.com>
2022-09-21 18:19:24 +0530

    Add UC7 Avoid duplicate Entries

--- Address_Book/AddressBook.cs
+++ Address_Book/AddressBook.cs
@@ -8,10 +8,16 @@ namespace Address_Book
 {
     class AddressBook
     {
-        public static List<NewMember> contactList = new List<NewMember>();
-        /// Adds the person.
-        public  static void AddaPerson()
+        public List<NewMember> contactList;
+
+        public AddressBook()
         {
+            contactList = new List<NewMember>();
+        }
+        /// Adds the person.
+        public bool AddaPerson()
+        {
+            bool found = false;
             NewMember newMember = new NewMember();
             Console.Write("Enter First Name: ");
             newMember.FirstName = Console.ReadLine();
@@ -29,10 +35,37 @@ namespace Address_Book
             newMember.PinCode = Console.ReadLine();
             Console.Write("Enter Email Id: ");
             newMember.EmailId = Console.ReadLine();
-            contactList.Add(newMember);
-        }      
+            var name = newMember.FirstName.ToLower() + newMember.LastName.ToLower();
+            if (contactList.Count == 0)
+            {
+                contactList.Add(newMember);
+            }
+            else
+            {
+
+                var firstname = contactList.Select(x => x.FirstName);
+                var lastname = contactList.Select(x => x.LastName);
+                foreach (var members in firstname.Zip(lastname, Tuple.Create))
+                {
+                    if (members.Item1 == newMember.FirstName && members.Item2 == newMember.LastName)
+                    {
+                        found = true;
+                        break;
+                    }
+                }
+                if (found)
+                {
+                    Console.WriteLine("Already Exists Cant Add");
+                }
+                else
+                {
+                    contactList.Add(newMember);
+                }
+            }
+            return found;
+        }
         /// Prints the person.
-        public static void PrintPerson(NewMember member)
+        public void PrintPerson(NewMember member)
         {
             Console.WriteLine("First Name: " + member.FirstName);
             Console.WriteLine("Last Name: " + member.LastName);
@@ -45,7 +78,7 @@ namespace Address_Book
             Console.WriteLine("");
         }
         /// Modifies the details.
-        public static void Modify()
+        public void Modify()
         {
             if (contactList.Count > 0)
             {
@@ -113,48 +146,216 @@ namespace Address_Book
             {
                 Console.WriteLine("Your Address Book is empty!");
             }
-            /// Deletes the details.        
-            public static void DeleteDetails()
+        }
+        /// Deletes the details.
+        public void DeleteDetails()
+        {
+            if (contactList.Count > 0)
             {
-                if (contactList.Count > 0)
+                Console.WriteLine("Enter the first name of the person to be deleted:");
+                string target = Console.ReadLine();
+                foreach (var member in contactList)
                 {
-                    Console.WriteLine("Enter the first name of the person to be deleted:");
-                    string target = Console.ReadLine();
-                    foreach (var member in contactList)
+                    if (member.FirstName.ToLower() == target.ToLower())
                     {
-                        if (member.FirstName.ToLower() == target.ToLower())
-                        {
-                            contactList.Remove(member);
-                            Console.WriteLine("Deleted Contact : " + member.FirstName);
-                            break;
-                        }
+                        contactList.Remove(member);
+                        Console.WriteLine("Deleted Contact : " + member.FirstName);
+                        break;
                     }
-                }
-                else
-                {
-                    Console.WriteLine("Your Address book is empty!");
                 }
             }
-            /// <summary>
-            /// Lists the contact people.
-            /// </summary>
-            public static void ListContactPeople()
+            else
             {
-                if (contactList.Count > 0)
+                Console.WriteLine("Your Address book is empty!");
+            }
+        }
+        /// Lists the contact people.
+        public void ListContactPeople()
+        {
+            if (contactList.Count > 0)
+            {
+                Console.WriteLine("The Contact List : ");
+                foreach (var member in contactList)
                 {
-                    Console.WriteLine("The Contact List : ");
-                    foreach (var member in contactList)
-                    {
-                        PrintPerson(member);
-                    }
+                    PrintPerson(member);
                 }
-                else
-                {
-                    Console.WriteLine("Your address book is empty.");
-                    return;
-                }
+            }
+            else
+            {
+                Console.WriteLine("Your address book is empty.");
+                return;
             }
         }
     }
+    //class AddressBook
+    //{
+    //    public static List<NewMember> contactList = new List<NewMember>();
+    //    /// <summary>
+    //    /// Adds the person.
+    //    /// </summary>
+    //    public static void AddaPerson()
+    //    {
+    //        NewMember newMember = new NewMember();
+    //        Console.Write("Enter First Name: ");
+    //        newMember.firstname = Console.ReadLine();
+    //        Console.Write("Enter Last Name: ");
+    //        newMember.lastname = Console.ReadLine();
+    //        Console.Write("Enter Phone Number: ");
+    //        newMember.phonenumber = Console.ReadLine();
+    //        Console.Write("Enter Address: ");
+    //        newMember.Address = Console.ReadLine();
+    //        Console.Write("Enter City: ");
+    //        newMember.City = Console.ReadLine();
+    //        Console.Write("Enter State: ");
+    //        newMember.State = Console.ReadLine();
+    //        Console.Write("Enter Pincode: ");
+    //        newMember.pincode = Console.ReadLine();
+    //        contactList.Add(newMember);
+    //    }
+    //    /// <summary>
+    //    /// Prints the person.
+    //    /// </summary>
+    //    public static void PrintPerson(NewMember member)
+    //    {
+    //        Console.WriteLine("First Name: " + member.firstname);
+    //        Console.WriteLine("Last Name: " + member.lastname);
+    //        Console.WriteLine("Phone Number: " + member.phonenumber);
+    //        Console.WriteLine("Address " + member.Address);
+    //        Console.WriteLine("City: " + member.City);
+    //        Console.WriteLine("State: " + member.State);
+    //        Console.WriteLine("Pincode: " + member.pincode);
+    //        Console.WriteLine("");
+
+    //    }
+    //    /// <summary>
+    //    /// Modifies the details.
+    //    /// </summary>
+    //    public static void Modify()
+    //    {
+    //        if (contactList.Count > 0)
+    //        {
+    //            Console.WriteLine("Enter the First name of the contact to modify:");
+    //            string target = Console.ReadLine();
+    //            foreach (var member in contactList)
+    //            {
+    //                if (member.firstname.ToLower() == target.ToLower())
+    //                {
+    //                    while (true)
+    //                    {
+    //                        Console.WriteLine("Enter the option to modify the property: ");
+    //                        Console.WriteLine("Enter 1 to Change First name ");
+    //                        Console.WriteLine("Enter 2 to Change Last name ");
+    //                        Console.WriteLine("Enter 3 to Change Phone Number ");
+    //                        Console.WriteLine("Enter 4 to Change Address ");
+    //                        Console.WriteLine("Enter 5 to Change City ");
+    //                        Console.WriteLine("Enter 6 to Change State ");
+    //                        Console.WriteLine("Enter 7 to Change Pincode ");
+    //                        Console.WriteLine("Enter 8 to Exit ");
+    //                        int option = Convert.ToInt32(Console.ReadLine());
+    //                        switch (option)
+    //                        {
+    //                            case 1:
+    //                                Console.WriteLine("Enter the New First Name: ");
+    //                                member.firstname = Console.ReadLine();
+    //                                break;
+    //                            case 2:
+    //                                Console.WriteLine("Enter the New Last Name: ");
+    //                                member.lastname = Console.ReadLine();
+    //                                break;
+    //                            case 3:
+    //                                Console.WriteLine("Enter the New Phone Number: ");
+    //                                member.phonenumber = Console.ReadLine();
+    //                                break;
+    //                            case 4:
+    //                                Console.WriteLine("Enter the New Address: ");
+    //                                member.Address = Console.ReadLine();
+    //                                break;
+    //                            case 5:
+    //                                Console.WriteLine("Enter the New City: ");
+    //                                member.City = Console.ReadLine();
+    //                                break;
+    //                            case 6:
+    //                                Console.WriteLine("Enter the New State: ");
+    //                                member.State = Console.ReadLine();
+    //                                break;
+    //                            case 7:
+    //                                Console.WriteLine("Enter the New Pin Code: ");
+    //                                member.pincode = Console.ReadLine();
+    //                                break;
+    //                            case 8:
+    //                                return;
+
+    //                        }
+
+    //                    }
+
+    //                }
+    //                else
+    //                {
+    //                    Console.WriteLine("Please enter the valid name!");
+    //                }
+
+    //            }
+
+
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("Your Address Book is empty!");
+    //        }
+
+    //    }
+    //    /// <summary>
+    //    /// Deletes the details.
+    //    /// </summary>
+    //    public static void DeleteDetails()
+    //    {
+    //        if (contactList.Count > 0)
+    //        {
+    //            Console.WriteLine("Enter the first name of the person to be deleted:");
+    //            string target = Console.ReadLine();
+    //            foreach (var member in contactList)
+    //            {
+    //                if (member.firstname.ToLower() == target.ToLower())
+    //                {
+    //                    contactList.Remove(member);
+    //                    Console.WriteLine("Deleted Contact : " + member.firstname);
+    //                    break;
+
+    //                }
+    //                else
+    //                {
+    //                    Console.WriteLine("Not Available.");
+    //                }
+    //            }
+    //        }
+    //        else
+    //        {
+    //            Console.WriteLine("Your Address book is empty!");
+    //        }
+    //    }
+    //    /// <summary>
+    //    /// Lists the contact people.
+    //    /// </summary>
+    //    public static void ListContactPeople()
+    //    {
+    //        if (contactList.Count > 0)
+    //        {
+    //            Console.WriteLine("The Contact List : ");
+    //            foreach (var member in contactList)
+    //            {
+    //                PrintPerson(member);
+    //            }
+
+    //        }
+    //        else
+    //        {
+
+    //            Console.WriteLine("Your address book is empty.");
+    //            return;
+    //        }
+    //    }
+
+    //}
 }
 
--- Address_Book/Program.cs
+++ Address_Book/Program.cs
@@ -9,7 +9,6 @@ namespace Address_Book
         public static Dictionary<string, List<NewMember>> addressbooknames = new Dictionary<string, List<NewMember>>();
         static void Main(string[] args)
         {
-
             Console.WriteLine("Welcome to Address Book System!");
             Console.WriteLine("Enter the number of address books to be added to the system: ");
             int addressbooks = Convert.ToInt32(Console.ReadLine());
@@ -26,10 +25,13 @@ namespace Address_Book
                 while (noofcontatcs != 0)
                 {
                     Console.WriteLine("Enter the details of contact to be added: ");
-                    addressBook.AddaPerson();
-                    noofcontatcs--;
-                    Console.WriteLine(" ");
-                    //addressBook.ListContactPeople();
+                    bool found = addressBook.AddaPerson();
+                    if (!found)
+                    {
+                        noofcontatcs--;
+                        Console.WriteLine(" ");
+                        addressBook.ListContactPeople();
+                    }
                 }
                 Console.WriteLine("To Modify Details Press 1/ To delete a contact detail Press 2/Else Press 0");
                 int option = Convert.ToInt32(Console.ReadLine());
@@ -37,13 +39,13 @@ namespace Address_Book
                 {
                     addressBook.Modify();
                     Console.WriteLine(" ");
-                   // addressBook.ListContactPeople();
+                    addressBook.ListContactPeople();
                 }
                 else if (option == 2)
                 {
-                    //addressBook.DeleteDetails();
+                    addressBook.DeleteDetails();
                     Console.WriteLine(" ");
-                    //addressBook.ListContactPeople();
+                    addressBook.ListContactPeople();
                 }
                 if (addressbooknames.ContainsKey(addressbookname))
                 {
@@ -64,4 +66,101 @@ namespace Address_Book
             }
         }
     }
+    //class Program
+    //{
+    //    //Declared Dictionary
+    //    public static Dictionary<string, List<NewMember>> addressbooknames = new Dictionary<string, List<NewMember>>();
+    //    static void Main(string[] args)
+    //    {
+
+    //        Console.WriteLine("Welcome to Address Book System!");
+    //        Console.WriteLine("Enter the number of address books to be added to the system: ");
+    //        int addressbooks = Convert.ToInt32(Console.ReadLine());
+    //        int noofbooksadded = 0;
+    //        while (noofbooksadded < addressbooks)
+    //        {
+    //            Console.WriteLine("Enter the address book name: ");
+    //            string addressbookname = Console.ReadLine();
+    //            ///obj creation
+    //            AddressBook addressBook = new AddressBook();
+    //            Console.WriteLine("Enter the no of contacts in the address book: ");
+    //            int noofcontatcs = Convert.ToInt32(Console.ReadLine());
+
+    //            while (noofcontatcs != 0)
+    //            {
+    //                Console.WriteLine("Enter the details of contact to be added: ");
+    //                addressBook.AddaPerson();
+    //                noofcontatcs--;
+    //                Console.WriteLine(" ");
+    //                //addressBook.ListContactPeople();
+    //            }
+    //            Console.WriteLine("To Modify Details Press 1/ To delete a contact detail Press 2/Else Press 0");
+    //            int option = Convert.ToInt32(Console.ReadLine());
+    //            if (option == 1)
+    //            {
+    //                addressBook.Modify();
+    //                Console.WriteLine(" ");
+    //               // addressBook.ListContactPeople();
+    //            }
+    //            else if (option == 2)
+    //            {
+    //                //addressBook.DeleteDetails();
+    //                Console.WriteLine(" ");
+    //                //addressBook.ListContactPeople();
+    //            }
+    //            if (addressbooknames.ContainsKey(addressbookname))
+    //            {
+    //                Console.WriteLine("Existing address book name : {0} . Please retry!", addressbookname);
+    //                return;
+    //            }
+    //            else
+    //            {
+    //                ///adding details to the dictionary
+    //                addressbooknames.Add(addressbookname, addressBook.contactList);
+    //            }
+    //            noofbooksadded++;
+    //        }
+    //        //using keyvalue to get value of the key.
+    //        foreach (KeyValuePair<string, List<NewMember>> kvp in addressbooknames)
+    //        {
+    //            Console.WriteLine("The address Books are:{0}", kvp.Key);
+    //        }
+    //    }
+    //class Program
+    //{
+    //    static void Main(string[] args)
+    //    {
+
+    //        Console.WriteLine("Welcome to Address Book System!");
+    //        while (true)
+    //        {
+    //            Console.WriteLine("Enter an Option:");
+    //            Console.WriteLine("Enter 1 to Add a Member in a contact list");
+    //            Console.WriteLine("Enter 2 to Print the Member in contact list");
+    //            Console.WriteLine("Enter 3 to Modify the contact details");
+    //            Console.WriteLine("Enter 4 to Delete the contact details");
+    //            Console.WriteLine("Enter 5 to Exit");
+    //            int option = Convert.ToInt32(Console.ReadLine());
+    //            switch (option)
+    //            {
+    //                case 1:
+    //                    AddressBook.AddaPerson();
+    //                    break;
+    //                case 2:
+    //                    AddressBook.ListContactPeople();
+    //                    break;
+    //                case 3:
+    //                    AddressBook.Modify();
+    //                    break;
+    //                case 4:
+    //                    AddressBook.DeleteDetails();
+    //                    break;
+    //                case 5:
+    //                    return;
+
+    //            }
+    //        }
+
+    //    }
+    //}
 }
