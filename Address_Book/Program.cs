49819c4cb1e0e59fb0f211d0e9415a8ccf58b3e0
“preetimca2016” <preetimca2016@gmail.com>
2022-09-21 15:51:26 +0530

    Add Program file

---
+++ Address_Book/Program.cs
@@ -0,0 +1,28 @@
+﻿namespace Address_Book
+{
+    internal class Program
+    {
+        static void Main(string[] args)
+        {
+            Console.WriteLine("Welcome to Address Book System!");
+            while (true)
+            {
+                Console.WriteLine("Enter an Option:");
+                Console.WriteLine("Enter 1 to Add a Member in a contact list:");
+                Console.WriteLine("Enter 2 to Print the Member in contact list:");
+                int option = Convert.ToInt32(Console.ReadLine());
+                switch (option)
+                {
+                    case 1:
+                        AddressBook.AddaPerson();
+                        break;
+                    case 2:
+                        AddressBook.ListContactPeople();
+                        break;
+                    case 3:
+                        return;
+                }
+            }
+        }
+    }
+}
